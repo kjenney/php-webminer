764cabf8a93e1c445160072bb46a558e94a12cd6
Ken <kjenney@gmail.com>
2015-03-01 14:22:50 -0500

    Updated to jobs xml

--- example.php
+++ example.php
@@ -4,7 +4,7 @@
 require_once('vendor/autoload.php');
 
 $config = new Config();
-$config->setXML("jobs/yahoo_trending.xml");
+$config->setJobXML("jobs/yahoo_trending.xml");
 $port = '4444';   // This is the default port
 
 $mine = new Miner($port,"test");
